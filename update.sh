b6fa01da099ff86f1848844f3da80c6b84ff70d2
ikbenignace <ikbenignace@live.be>
2023-02-16 12:57:53 +0100

    a

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
-docker stack deploy traefik --compose-file traefik.yml
+docker stack deploy proxy --compose-file nginx.yml
+#docker stack deploy traefik --compose-file traefik.yml
 docker stack deploy portainer --compose-file portainer.yml
